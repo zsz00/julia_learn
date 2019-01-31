721813079f1da190a23b92e9a6e26e2cf33d14ed
zsz00 <6174@gmail.com>
2019-01-31 22:59:54 +0800

    13

--- cv/load_img_test.jl
+++ cv/load_img_test.jl
@@ -1,25 +1,34 @@
 # test image read ,  ssim
 using Images  #, ImageView
+using PyCall
+cv2 = pyimport("cv2")
 
-function ssim(img1, img2)
+function julia_img(img1)
     im1 = load(img1);
-    im1 = Gray.(im1);
-    im1 = imresize(im1, (300, 300));
-    im2 = load(img2);
-    im2 = Gray.(im2);
-    im2 = imresize(im2, (300, 300));
+    # im1 = Gray.(im1);
+    # im1 = imresize(im1, (300, 300));
+    return 0
+#     imshow(im1)
+end
+
+function python_img(img1)
+    img = cv2.imread(img1) 
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = cv2.resize(img, (300, 300), interpolation=cv2.INTER_CUBIC)
     return 0
 #     imshow(im1)
 end
 
 
-img1 = raw"C:\zsz\ML\code\DL\img_tools\test\img\2\2.jpg"
-img2 = raw"C:\zsz\ML\code\DL\img_tools\test\img\2\00025.jpg"
+img1 = s"stitcher\test\imgs\1.jpg"
 # img1 = "/root/julia/imgs/00000000210000000_02868.jpg"  # stata-1.png
-# img2 = "/root/julia/imgs/00000000210000000_02928.jpg"
 
 @time for i in 1:10
-    ssim(img1, img2)
+    julia_img(img1)
+end
+
+@time for i in 1:10
+    python_img(img1)
 end
 
 
@@ -34,7 +43,11 @@ python3 : time: 0.19165117740631105 s
 time: 0.03723652362823486 s
 time: 0.06383512020111085 s
 
-0.6096748
+------------
+  7.030900 seconds (10.27 M allocations: 608.996 MiB, 4.43% gc time)
+  9.890474 seconds (116.12 M allocations: 4.527 GiB, 10.21% gc time)
 
+  4.989357 seconds (6.07 M allocations: 378.117 MiB, 3.98% gc time)
+  5.844481 seconds (83.96 M allocations: 3.373 GiB, 7.90% gc time)
 =#
 

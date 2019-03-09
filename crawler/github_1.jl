e1e34e50115c228d932f9e18c67e43ccb7cce386
zsz00 <6174@gmail.com>
2019-03-09 23:10:10 +0800

    13

--- crawler/github_1.jl
+++ crawler/github_1.jl
@@ -7,7 +7,8 @@ using DataFrames
 
 function stars()
     # url_1 = "https://github.com/search?l=r&q=stars%3A%3E1000&s=updated&type=Repositories"
-    langs = ["matlab", "r", "lua", "javascript", "swift", "php", "c", "cpp", "java"]  # ["python", "julia", "rust", "typescript", "go"]
+    langs = ["matlab", "r", "lua", "javascript", "swift", "php", "c", "cpp", "java", "python", "julia", "rust", "typescript", "go"]
+    sort!(langs)
     all = []
     for pl in langs
         print(pl, "\t")
@@ -33,10 +34,15 @@ function stars()
                 star = 1
             else
                 star = data[1]
+                try
                 aa = star.match   # 匹配项字符串
                 star = match(r"\d*,*\d+", aa).match
                 star = replace(star, ","=>"")
                 star = parse(Int, star)
+                catch
+                    print("star: ", star)
+                    star = 0
+                end
             end
             push!(lang, star)
             print(" ", star)
@@ -54,61 +60,11 @@ print(data)
 
 
 #=
-20190220
-python 10  star num: RegexMatch("    91,792 repository results")
-python 100  star num: RegexMatch("    16,660 repository results")
-python 1000  star num: RegexMatch("    1,907 repository results")
-python 10000  star num: RegexMatch("    80 repository results")
-julia 10  star num: RegexMatch("    1,185 repository results")
-julia 100  star num: RegexMatch("    135 repository results")
-julia 1000  star num: RegexMatch("    5 repository results")
-julia 10000  star num: 0
-rust 10  star num: RegexMatch("    5,655 repository results")
-rust 100  star num: RegexMatch("    1,169 repository results")
-rust 1000  star num: RegexMatch("    138 repository results")
-rust 10000  star num: RegexMatch("    6 repository results")
-typescript 10  star num: RegexMatch("    11,353 repository results")
-typescript 100  star num: RegexMatch("    2,345 repository results")
-typescript 1000  star num: RegexMatch("    367 repository results")
-typescript 10000  star num: RegexMatch("    28 repository results")
-go 10  star num: RegexMatch("    24,339 repository results")
-go 100  star num: RegexMatch("    5,890 repository results")
-go 1000  star num: RegexMatch("    998 repository results")
-go 10000  star num: RegexMatch("    58 repository results")
-matlab 10  star num: RegexMatch("    2,064 repository results")
-matlab 100  star num: RegexMatch("    198 repository results")
-matlab 1000  star num: RegexMatch("    11 repository results")
-matlab 10000  star num: 0
-r 10  star num: RegexMatch("    4,895 repository results")
-r 100  star num: RegexMatch("    574 repository results")
-r 1000  star num: RegexMatch("    25 repository results")
-r 10000  star num: 0
-lua 10  star num: RegexMatch("    4,002 repository results")
-lua 100  star num: RegexMatch("    619 repository results")
-lua 1000  star num: RegexMatch("    55 repository results")
-lua 10000  star num: RegexMatch("    4 repository results")
-javascript 10  star num: RegexMatch("    150,292 repository results")
-javascript 100  star num: RegexMatch("    29,737 repository results")
-javascript 1000  star num: RegexMatch("    4,387 repository results")
-javascript 10000  star num: RegexMatch("    346 repository results")
-swift 10  star num: RegexMatch("    13,137 repository results")
-swift 100  star num: RegexMatch("    3,436 repository results")
-swift 1000  star num: RegexMatch("    580 repository results")
-swift 10000  star num: RegexMatch("    27 repository results")
-php 10  star num: RegexMatch("    39,750 repository results")
-php 100  star num: RegexMatch("    6,300 repository results")
-php 1000  star num: RegexMatch("    750 repository results")
-php 10000  star num: RegexMatch("    20 repository results")
-c 10  star num: RegexMatch("    31,434 repository results")
-c 100  star num: RegexMatch("    5,780 repository results")
-c 1000  star num: RegexMatch("    665 repository results")
-c 10000  star num: RegexMatch("    21 repository results")
-cpp 10  star num: RegexMatch("    35,763 repository results")
-cpp 100  star num: RegexMatch("    6,401 repository results")
-cpp 1000  star num: RegexMatch("    760 repository results")
-cpp 10000  star num: RegexMatch("    43 repository results")
-java 10  star num: RegexMatch("    65,168 repository results")
-java 100  star num: RegexMatch("    13,627 repository results")
-java 1000  star num: RegexMatch("    2,009 repository results")
-java 10000  star num: RegexMatch("    69 repository results")
+20190309
+│ 1   │ matlab │ r    │ lua  │ javascript │ swift │ php   │ c     │ cpp   │ java  │ python │ julia │ rust │ typescript │ go    │
+│ 2   │ 2111   │ 4986 │ 4039 │ 151659     │ 13290 │ 40095 │ 31790 │ 36326 │ 65818 │ 93305  │ 1211  │ 5804 │ 11716      │ 24676 │
+│ 3   │ 199    │ 584  │ 626  │ 30007      │ 3480  │ 6338  │ 5852  │ 6518  │ 13767 │ 16949  │ 138   │ 1204 │ 2417       │ 5975  │
+│ 4   │ 11     │ 25   │ 57   │ 4412       │ 585   │ 762   │ 673   │ 778   │ 2036  │ 1949   │ 5     │ 142  │ 385        │ 1021  │
+│ 5   │ 1      │ 1    │ 4    │ 345        │ 27    │ 20    │ 21    │ 43    │ 70    │ 82     │ 1     │ 6    │ 29         │ 60    │
+
 =#

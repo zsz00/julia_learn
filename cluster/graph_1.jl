552602378b224812dcb599c6006db41419493be4
zsz00 <zsz6174@gmail.com>
2019-12-09 21:31:42 +0800

    1

--- cluster/graph_1.jl
+++ cluster/graph_1.jl
@@ -4,11 +4,12 @@ using Dates
 using ProgressMeter
 using LinearAlgebra
 using Statistics
+# using ProfileView
 
 
 function cluster_1()
     """
-    动态的 层次聚类. 算方差
+    增量的 层次聚类. 算方差
     used: 14845.068 s, 70184.  4h on 10.42.64.84
     g_list:(3358,)
     
@@ -45,6 +46,45 @@ function cluster_1()
     println("g_list:", size(g_list))
 end
 
+function cluster_1_2()
+    """
+    增量的 层次聚类. 算方差
+    used: 14845.068 s, 70184.  4h on 10.42.64.84
+    g_list:(3358,)
+
+    1:33
+
+    """
+    t0 = Dates.now()
+    G = SimpleGraph()  # Graph()
+    mg = G  # MetaGraph(G)
+    # feats = npzread(raw"C:\zsz\ML\code\DL\face_cluster\face_cluster\tmp2\data\valse19.npy")
+    feats = npzread("/data5/yongzhang/cluster/data/cluster_data/valse/valse_feat.npy")  # [1:20000,1:end]  # 7w*384 2s  比np.load()的慢
+
+    size_1 = size(feats)[1]
+    t1 = Dates.now()
+    println("used: ", (t1 - t0).value/1000, "s, ", size_1)
+    p = Progress(size_1)
+    for i=1:size_1    # n*(n-1)/2.    并行崩溃   Threads.@threads 
+        add_vertex!(mg)
+        next!(p)
+        for j=1:i-1   # nodes 是一个嵌套字典结构   @inbounds
+            feat_1 = feats[j, 1:end]  # get_prop(mg, j, :feat)   # nodes_1[j]["feat"]
+            # cos = sum(feats[i, 1:end] .* feat_1)   # 慢
+            cos = feats[i, 1:end]' * feat_1   # 慢
+            # println(join([i, j, cos], ", "))
+            if cos > 0.5
+                add_edge!(mg, i, j)   # 并行崩溃, 不是原子操作 
+            end
+        end
+    end
+    t2 = Dates.now()
+    println("used: ", (t2 - t1).value/1000, "s, ", size_1)
+
+    g_list = connected_components(mg)
+    println("g_list:", size(g_list))
+end
+
 
 function cluster_2()
     t0 = Dates.now()
@@ -56,14 +96,17 @@ function cluster_2()
     size_1 = size(feats)[1]
     t1 = Dates.now()
     println("used: ", (t1 - t0).value/1000, " s, ", size_1)
+    p = Progress(size_1)
     Threads.@threads for i in range(1, stop=size_1)    # n*(n-1)/2.   @showprogress
         add_vertex!(mg)   #  
         set_props!(mg, i, Dict(:feat=>feats[i,1:end]))  # feats可以不存储在图里,可以存储在外边,可以用节点号索引.
+        # sleep(1)
+        next!(p)
         # nodes_1 = vertices(mg)
         # println("nodes:", collect(nodes_1))  # 打印每个node的key/id.
 
         for key in range(1, stop=i-1)   # nodes 是一个嵌套字典结构
-            feat_1 = get_prop(mg, key, :feat)   # 在图中查找节点数据. 按字典方式查.hash查找.
+            feat_1 = feats[key, 1:end]  # get_prop(mg, key, :feat)   # 在图中查找节点数据. 按字典方式查.hash查找. 竟然不能并行
             cos = feats[i, 1:end]' * feat_1   # 一个一个的算, 没有用矩阵运算. 可以换成矩阵运算.
             # println(join([i, key, cos], ", "))
             if cos > 0.5
@@ -74,7 +117,7 @@ function cluster_2()
             end
         end
         # println("nv(mg):", nv(mg))  # mg的节点数量
-        gg, var = get_gg(mg, i)   # 找到包含当前node的子图
+        # gg, var = get_gg(mg, i)   # 找到包含当前node的子图
         # println("i:", i, " var:", var)
 
         # 递归处理 var大的簇. 动态阈值.  怎么递归,循环
@@ -82,7 +125,8 @@ function cluster_2()
     end
     t2 = Dates.now()
     println("used: ", (t2 - t1).value/1000, " s, ", size_1)
-
+    g_list = connected_components(mg)
+    # println("g_list:", size(g_list)[1])    # 子图的数量
 end
 
 
@@ -97,20 +141,21 @@ function cluster_3()
     t1 = Dates.now()
     println("used: ", (t1 - t0).value/1000, " s, ", size_1)
     feats_1 = []
+    th = 0.5
     for i in range(1, stop=size_1)    # n*(n-1)/2.   @showprogress
         add_vertex!(mg)   #  
         set_props!(mg, i, Dict(:feat=>feats[i,1:end]))  # feats可以不存储在图里,可以存储在外边,可以用节点号索引.
         # nodes_1 = vertices(mg)
         # println("nodes:", collect(nodes_1))  # 打印每个node的key/id.
-        if feats_1 == []:
-            push!(feats_1, feats[i,1:end])
-            continue
-        cos = feats_1 * feats[i].T
         push!(feats_1, feats[i,1:end])
-
+        feats_1 = vcat((hcat(i...) for i in feats_1)...)  # 转换 shape
+        println(size(feats_1), size(feats[i,1:end]))
+        cos =  feats_1 * feats[i,1:end]
+        println(size(cos))
         idx_1 = findall(cos.>th)  # 很慢
-        aa = [{"weight": cos} for cos in cos[idx]]
-        
+        # aa = [{"weight": cos} for cos in cos[idx]]
+        println(size(idx_1))
+        continue
         add_edges!(mg, i, key)    # 怎么批量加edges ???  找不见  . 自己写个循环吧
 
         # println("nv(mg):", nv(mg))  # mg的节点数量
@@ -163,7 +208,10 @@ function get_gg(mg, i)
 end
 
 
-cluster_2()
+# cluster_1_2()
+cluster_3()
+# @profview cluster_1_2()
+# ProfileView.svgwrite("profile_results.svg")
 
 
 

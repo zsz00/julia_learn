f0928588e5fccfcf26b0c5e57f23c106975de6ab
yongzhang <yongzhang@deepglint.com>
2021-05-15 22:11:52 +0800

    updae

--- db/db_2.jl
+++ db/db_2.jl
@@ -1,6 +1,8 @@
 using Octo
 using Octo.Adapters.PostgreSQL
 using DataFrames
+using StatsPlots
+
 
 
 println(Repo.debug_sql())
@@ -19,7 +21,10 @@ a = Repo.query(Raw("""select * from task;"""))   # query execute
 b = Repo.query(Raw("""select obj_info.obj_id,obj_info.blur,task_object.reason from face as obj_info, task_object 
 where obj_info.obj_id=task_object.obj_id and obj_info.dataset_name='af_nj_344w_2730' and task_object.obj_type=5 
 and task_object.person_id='' and task_object.task_id='b500627e-58c4-4ead-aa72-53f7d404bf48' limit 100;"""))
-println(DataFrame(b))
+bb = DataFrame(b)
+println(bb)
+
+@df bb histogram(:blur)
 
 
 #=
